f9cbf12bac476984955cd7d12a1eeb5d1a89a21a
Lars Petersen <petersen.l.lars@gmail.com>
2016-11-23 22:43:53 +0100

    Funktionen fuer den Spielablauf

---
+++ game.py
@@ -0,0 +1,39 @@
+"""..."""
+
+__author__ = "6360278: Qasim Raza, 6290157: Lars Petersen"
+__copyright__ = ""
+__credits__ = "" 
+__email__ = "qasimr@icloud.com, petersen@informatik.uni-frankfurt.de"
+
+
+def set_towns():
+    """..."""
+
+    return [None]
+    
+
+
+def set_managers(towns):
+    """..."""
+
+    return [None]
+    
+
+
+def set_timeframe():
+    """..."""
+
+    pass
+
+
+def set_potentials(towns):
+    """..."""
+
+    return [None]
+
+
+def set_network(towns):
+    """..."""
+
+    return [[None]]
+
