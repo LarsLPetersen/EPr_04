2dcb689b1ade3ec55d6ce83403bcda397a109738
Lars Petersen <petersen.l.lars@gmail.com>
2016-11-25 16:01:13 +0100

    Stub wird als Test-Version fixiert

---
+++ game_test.py
@@ -0,0 +1,39 @@
+"""..."""
+
+__author__ = "6360278: Qasim Raza, 6290157: Lars Petersen"
+__copyright__ = ""
+__credits__ = "" 
+__email__ = "qasimr@icloud.com, petersen@informatik.uni-frankfurt.de"
+
+
+def set_towns():
+    """returns n-tuple, elements pairewise different strings of length > 0"""
+
+    return ("town1", "town2")
+    
+
+
+def set_managers(towns):
+    """returns a 2-tuple (hometown, m) where m = # managers"""
+
+    return ("town1", 5)
+    
+
+
+def set_timeframe():
+    """returns d"""
+
+    return 20
+    
+
+def set_potentials(towns):
+    """returns n-tuple with values p_i"""
+
+    return (-20, 90)
+
+
+def set_network(towns):
+    """retruns n-tuple of n-tuples with values s_ij"""
+
+    return ((1, 1), (1, 1))
+
